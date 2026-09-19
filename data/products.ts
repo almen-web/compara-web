aebdcd707a260977135f653a38c247fd3dbb120b
almen-web <kevinvicentearmenteros@gmail.com>
2026-09-19 12:23:07 +0200

    Fix: corregir imagenes exactas de iPhone 16 y 17

--- data/products.ts
+++ data/products.ts
@@ -187,7 +187,7 @@ export const products: Product[] = [
     name: "Apple iPhone 16 128GB (Nuevo)",
     category: "Móviles",
     rating: "4.8",
-    image: "https://m.media-amazon.com/images/I/61bK6PMOC3L._AC_SL1500_.jpg",
+    image: "https://m.media-amazon.com/images/I/611aO811MFL._AC_SL1500_.jpg",
     offers: [
       {
         id: "iphone16-nuevo-amazon",
@@ -228,7 +228,7 @@ export const products: Product[] = [
     name: "Apple iPhone 16 128GB (Reacondicionado)",
     category: "Móviles",
     rating: "4.6",
-    image: "https://m.media-amazon.com/images/I/61bK6PMOC3L._AC_SL1500_.jpg",
+    image: "https://m.media-amazon.com/images/I/611aO811MFL._AC_SL1500_.jpg",
     offers: [
       {
         id: "iphone16-reacondicionado-pccomponentes",
@@ -258,7 +258,7 @@ export const products: Product[] = [
     name: "Apple iPhone 17 256GB (Nuevo)",
     category: "Móviles",
     rating: "4.8",
-    image: "https://m.media-amazon.com/images/I/71wLpWv1zML._AC_SL1500_.jpg",
+    image: "https://m.media-amazon.com/images/I/61+9Y3QdE8L._AC_SL1500_.jpg",
     offers: [
       {
         id: "iphone17-nuevo-mediamarkt",
@@ -288,7 +288,7 @@ export const products: Product[] = [
     name: "Apple iPhone 17 256GB (Reacondicionado)",
     category: "Móviles",
     rating: "4.8",
-    image: "https://m.media-amazon.com/images/I/71wLpWv1zML._AC_SL1500_.jpg",
+    image: "https://m.media-amazon.com/images/I/61+9Y3QdE8L._AC_SL1500_.jpg",
     offers: [
       {
         id: "iphone17-reacondicionado-amazon",
